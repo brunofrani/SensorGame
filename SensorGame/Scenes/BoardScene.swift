ca830faa06995437356083bc4bf56eb361dea5ab
Bruno Frani <brunifranigmail.com>
2021-10-02 00:41:02 +0200

    add ball resource

---
+++ SensorGame/Scenes/BoardScene.swift
@@ -0,0 +1,21 @@
+//
+//  MainScene.swift
+//  SensorGame
+//
+//  Created by Bruno Frani on 10/1/21.
+//
+
+import SpriteKit
+
+class MainScene: SKScene {
+    
+  var woodBoard: SKTileMapNode!
+
+    
+    override func didMove(to view: SKView) {
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+    }
+  
+}
